44757a1d651b22822906004b7f4435eba56ca7cd
hdcarr87 <hdmanley87@gmail.com>
2020-02-05 12:00:50 -0700

    updated routes

--- routes/db.js
+++ routes/db.js
@@ -4,25 +4,42 @@ const Workout = require("../models/workout.js");
 router.post("/api/workout", (req, res) => {
     Workout.create({})
     .then(dbWorkout => {
-        return Workout.findOneAndUpdate({_id: dbWorkout.id}, {$push: {exercises: req.body}});
-    }).then(result => {
-        res.json(result);
+        res.json(dbWorkout);
     }).catch(err => {
-        res.status(400).json(err);
-    })
-})
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } }
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      })
+  });
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
+    .catch(err => {
+        res.json(err);
+    })
 })
 
 router.get("/api/workouts/range", (req,res) => {
-    Workout.findOneAndDelete({})
+    Workout.find({})
+    .sort({ date: -1})
     .then(dbWorkout => {
         res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err);
     })
 })
 
